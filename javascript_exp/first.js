fc41d2dc9cac7422bf4227d6d09eab370772a934
Mmacedo96 <Miguelmacedo13@yahoo.com>
2019-12-15 08:23:32 -0500

    Commiting from VS code

--- javascript_exp/first.js
+++ javascript_exp/first.js
@@ -2,7 +2,12 @@ function foo(bar) {
     return bar;
 }
 
+function oneGreen(twoBlue) {
+    return twoBlue + " foo";
+}
+
 var x = foo("bar");
 console.log(x + " and foo");
 console.log("foo and" + x);
 console.log(x);
+console.log(oneGreen("bar"));
